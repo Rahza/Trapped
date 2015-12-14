c6087652cf98311b9ae7740739f5072cc10d7710
Alexander Seipel <alexander.seipel@gmail.com>
2015-12-14 16:31:26 +0100

    Final

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightManager : MonoBehaviour {
+
+    // Static reference to the instance
+    public static LightManager instance = null;
+
+    // Target of the light
+    private Transform target;
+    
+    // Should the light keep following the target or just focus it once?
+    private bool follow = false;
+
+    // Reference to the Light component
+    private Light lightComponent;
+
+    // Save the intensity of the light
+    private float baseIntensity;
+
+	void Start () {
+        // Get the component
+        lightComponent = GetComponent<Light>();
+
+        // Save the intensity value
+        baseIntensity = lightComponent.intensity;
+
+        // Initialise the instance variable
+        instance = this;
+	}
+	
+	void Update () {
+        // If follow is true, rotate the light towards the current target
+        if (follow) transform.LookAt(target);
+	}
+
+    // Set the target of the light
+    public void SetTarget(Transform target)
+    {
+        Reset();
+
+        follow = true;
+        this.target = target;
+    }
+
+    // Focus a target (once, do not follow it)
+    public void FocusTarget(Vector3 target)
+    {
+        Reset();
+
+        follow = false;
+        transform.LookAt(target);
+    }
+
+    // Set the intensity of the light (intensity being treated as a procentual value)
+    public void SetIntensity(float intensity)
+    {
+        lightComponent.intensity = intensity * baseIntensity;
+    }
+
+    // Reset the intensity of the light
+    private void Reset()
+    {
+        lightComponent.intensity = baseIntensity;
+    }
+}
--- Assets/Scripts/Pattern.cs
+++ Assets/Scripts/Pattern.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 
-public class Pattern {
+// Very simple class to create "Pattern" objects used by the "Blink Wall" - TRUE represents an opened eye, FALSE represents a closed eye
+public class Pattern
+{
 
     public bool left = true;
     public bool right = true;
 
-	public Pattern()
+    public Pattern()
     {
+        // Upon initialization, randomly assign a value of true or false to the left and right bools
         left = Random.Range(0, 2) > 0 ? true : false;
         right = Random.Range(0, 2) > 0 ? true : false;
     }
